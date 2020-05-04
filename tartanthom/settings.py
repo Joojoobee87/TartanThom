7c6560ca4b20955fc4e00b76c6afb646bfb51223
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-04 11:41:02 +0000

    added STATICFILES_DIRS path

--- tartanthom/settings.py
+++ tartanthom/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MEDIA_URL = '/media/'
