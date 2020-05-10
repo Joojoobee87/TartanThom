cf2366666a9ae3af0aa1a6545ff2ba32f20d1176
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-10 16:55:00 +0000

    added new account path to urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from accounts.views import user_registration, user_login, user_logout
+from accounts.views import user_registration, user_login, user_logout, user_account
 
 app_name = 'accounts'
 urlpatterns = [
     path('register/', user_registration, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('account/', user_account, name='account'),
 ]
