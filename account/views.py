5808891ed0ddf864f2ccbf55efa7c77a02176670
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-12 22:21:44 +0000

    integrate django auth

---
+++ account/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from account.forms import LoginForm, RegistrationForm
+
+# Create your views here.
+
+
+def user_registration(request):
+    """Register a new user"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = RegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+
+            user = authenticate(username=request.POST['username'],
+                                password=request.POST['password1'])
+            if user:
+                login(user=user, request=request)
+                messages.success(request, "A Tartan Thom account has been created for %s" % user.username)
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Sorry, we were unable to register your account")
+    else:
+        registration_form = RegistrationForm()
+    return render(request, 'accounts/register.html', {
+        "registration_form": registration_form})
+
+
+def user_login(request):
+    """User Login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method=="POST":
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            if user:
+                # Redirect to a success page.
+                login(user=user, request=request)
+                messages.success(request, "Welcome back to Tartan Thom")
+                return redirect(reverse('index'))
+            else:
+                # Return an 'invalid login' error message.
+                messages.error(request, "Sorry, we couldn't log you in. Please try again.")
+                return render(request, 'accounts/login.html', {"login_form": login_form})
+    else:
+        login_form = LoginForm()
+    return render(request, 'accounts/login.html', {"login_form": login_form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('index'))
+    # Redirect to a success page.
+
+
+@login_required()
+def user_account(request):
+    """Users Account"""
+    user = request.user
+    return render(request, 'accounts/account.html', {'user': user})
+
+
+@login_required()
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html', {'section': 'dashboard'})
