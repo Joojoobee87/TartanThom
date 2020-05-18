7e425d796c3dc1bed1cdf059c833b4ba056364eb
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-18 15:38:14 +0000

    added Products model to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Products
 
 # Register your models here.
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    model = Products
+    list_display = ('name', 'category', 'price', 'is_active')
+
+
+admin.site.register(Products, ProductsAdmin)
