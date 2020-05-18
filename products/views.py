61f7a127da910a54e5340a44a6a1803f25328ba9
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-18 15:40:08 +0000

    added views functions to return all products and products with specific product_type values

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from products.models import Products
 
 # Create your views here.
+
+
+def view_products(request):
+    """View all products in the collection"""
+    products = Products.objects.all()
+    return render(request, "products/products.html", {'products': products})
+
+
+def view_cards(request):
+    """View all products with the product type 'Cards' """
+    products = Products.objects.get(product_type='Cards')
+    return render(request, "products/products.html", {'products': products})
+
+
+def view_cake_toppers(request):
+    """View all products with the product type 'Cake Toppers' """
+    products = Products.objects.get(product_type='Cake Toppers')
+    return render(request, "products/products.html", {'products': products})
+
+
+def view_gifts(request):
+    """View all products with the product type 'Gifts' """
+    products = Products.objects.get(product_type='Gifts')
+    return render(request, "products/products.html", {'products': products})
