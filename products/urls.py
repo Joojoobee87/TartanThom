0e09eeb420899612ab050211aa6973458dc6cee7
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-19 13:38:53 +0000

    added view_product to urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from products.views import view_products, view_cards, view_cake_toppers, view_gifts
+from products.views import view_products, view_cards, view_cake_toppers, view_gifts, view_product
 
 
 app_name = 'products'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('cards/', view_cards, name='cards'),
     path('cake-toppers/', view_cake_toppers, name='cake-toppers'),
     path('gifts/', view_gifts, name='gifts'),
+    path('view-product/<id>/', view_product, name='view_product'),
 ]
