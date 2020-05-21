c2da60c49b4eb51e6f01a967326546077f88a8fc
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-21 10:34:40 +0000

    created new testimonials model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Testimonials(models.Model):
+    testimonial_date = models.DateField()
+    testimonial_allow_publish = models.BooleanField(default=True)
+    testimonial = models.TextField()
+    testimonial_active = models.BooleanField(default=False)
+    testimonial_user = models.ForeignKey(
+        User,
+        models.SET_NULL,
+        blank=True,
+        null=True,
+    )
