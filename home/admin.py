e0f5d54961d48a4dfeeddd4ab1c185e8ccbae2f6
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-21 10:41:12 +0000

    added TestimonialsAdmin class to register model in admin interface

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Testimonials
 
 # Register your models here.
+
+
+class TestimonialsAdmin(admin.ModelAdmin):
+    model = Testimonials
+    list_display = ('testimonial_user', 'testimonial_date', 'testimonial_active')
+
+
+admin.site.register(Testimonials, TestimonialsAdmin)
