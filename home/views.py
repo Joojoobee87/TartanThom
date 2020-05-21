375c26830cd10352045bfcb0e2c6eba59893688f
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-21 10:39:37 +0000

    updated index function to render all testimonials in index.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Testimonials
 
 # Create your views here.
 
 
 def index(request):
     """A view that displays the homepage"""
-    return render(request, "home/index.html")
+    testimonials = Testimonials.objects.all()
+    return render(request, "home/index.html", {'testimonials': testimonials})
