960645e6540f391fe0b0b9c779f19d006b4b1c60
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-22 12:21:33 +0000

    added product_type to functions

--- products/views.py
+++ products/views.py
@@ -7,25 +7,29 @@ from products.models import Products, ProductReviews
 def view_products(request):
     """View all products in the collection"""
     products = Products.objects.all()
-    return render(request, "products/products.html", {'products': products})
+    product_type = "All Products"
+    return render(request, "products/products.html", {'products': products, 'product_type': product_type})
 
 
 def view_cards(request):
     """View all products with the product type 'Cards' """
     products = Products.objects.filter(product_type='Cards')
-    return render(request, "products/products.html", {'products': products})
+    product_type = "Cards"
+    return render(request, "products/products.html", {'products': products, 'product_type': product_type})
 
 
 def view_cake_toppers(request):
     """View all products with the product type 'Cake Toppers' """
     products = Products.objects.filter(product_type='Cake Toppers')
-    return render(request, "products/products.html", {'products': products})
+    product_type = "Cake Toppers"
+    return render(request, "products/products.html", {'products': products, 'product_type': product_type})
 
 
 def view_gifts(request):
     """View all products with the product type 'Gifts' """
     products = Products.objects.filter(product_type='Gifts')
-    return render(request, "products/products.html", {'products': products})
+    product_type = "Gifts"
+    return render(request, "products/products.html", {'products': products, 'product_type': product_type})
 
 
 def view_product(request, id):
