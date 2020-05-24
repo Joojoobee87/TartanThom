ecaa7acff2553decfd1c6c228216b7c225e78a64
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-24 17:05:36 +0000

    added function to store session data for products in the basket

---
+++ basket/contexts.py
@@ -0,0 +1,23 @@
+from django.shortcuts import get_object_or_404
+from products.models import Products
+
+
+def basket_contents(request):
+    """Make the contents of the basket available for all pages"""
+
+    basket = request.session.get('basket', {})
+
+    basket_items = []
+    total = 0
+    product_count = 0
+
+    for id, quantity in basket.items():
+        product = get_object_or_404(Products, pk=id)
+        if product.sale_price:
+            total += quantity * product.sale_price
+        else:
+            total += quantity * product.price
+        product_count += quantity
+        basket_items.append({'id': id, 'quantity': quantity, 'product': product})
+
+    return {'basket_items': basket_items, 'total': total, 'product_count': product_count}
