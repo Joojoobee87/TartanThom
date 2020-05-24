57caaf6e2ae38fbee111203948a8e88183734053
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-24 17:06:58 +0000

    added functions to view basket and add items to the stored session data for products in the basket

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+
+
+def view_basket(request):
+    """A view that renders the contents of the basket"""
+    return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, id):
+    """A view that adds the quantity of a product to the basket"""
+    quantity = int(request.POST.get('quantity'))
+
+    basket = request.session.get('basket', {})
+
+    if id in basket:
+        basket[id] = int(basket[id]) + quantity
+    else:
+        basket[id] = basket.get(id, quantity)
+
+    request.session['basket'] = basket
+    return render(request, 'basket/basket.html')
+
+
+def amend_basket(request):
+    """A view that amends the quantity of a product in the basket"""
+    return render(request, 'basket/basket.html')
