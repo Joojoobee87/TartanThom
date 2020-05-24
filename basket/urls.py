0741db8a80d1c2d15c8c65a2e66188339dc7d1e4
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-24 19:58:48 +0000

    added url paths for amend_basket and delete_from_basket

--- basket/urls.py
+++ basket/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import view_basket, add_to_basket, amend_basket
+from .views import view_basket, add_to_basket, amend_basket, delete_from_basket
 
 
 app_name = 'basket'
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', view_basket, name='view_basket'),
     path('add/<id>/', add_to_basket, name='add_to_basket'),
     path('amend/<id>/', amend_basket, name='amend_basket'),
+    path('delete/<id>/', delete_from_basket, name='delete_from_basket'),
 ]
