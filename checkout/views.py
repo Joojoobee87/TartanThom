54cfdc362ca95ae8e837e80969e472eb6fcb4c5e
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-29 11:48:10 +0000

    imported and added login_required decorator to checkout function

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from checkout.forms import OrderForm
 
 # Create your views here.
 
 
+@login_required
 def checkout(request):
     order_form = OrderForm()
     return render(request, 'checkout/checkout.html', {'order_form': order_form})
