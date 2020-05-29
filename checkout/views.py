e5f8881020b93a8db7b15a36681451189fb8ce9a
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-29 11:56:49 +0000

    added if statement for request method is POST and order form is valid

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from checkout.forms import OrderForm
+from django.utils import timezone
+from django.contrib import messages
 
 # Create your views here.
 
 
 @login_required
 def checkout(request):
-    order_form = OrderForm()
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order = order_form.save()
+            order.date = timezone.now()
+        else:
+            print(order_form.errors)
+            messages.error(request, "Please review your details")
+    else:
+        order_form = OrderForm()
     return render(request, 'checkout/checkout.html', {'order_form': order_form})
