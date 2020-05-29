cf098d6a2bc6b6bec91368551f5381b94692a72a
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-29 11:39:49 +0000

    added checkout function to render order form in checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from checkout.forms import OrderForm
 
 # Create your views here.
+
+
+def checkout(request):
+    order_form = OrderForm()
+    return render(request, 'checkout/checkout.html', {'order_form': order_form})
