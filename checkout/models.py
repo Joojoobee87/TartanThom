6813abcd8c66e4724d7274948e36623f731e8c94
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-05-29 10:20:55 +0000

    created Order and Order Item models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from products.models import Products
 
 # Create your models here.
+
+class Order(models.Model):
+    fullname = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    date = models.DateField()
+    address_line1 = models.CharField(max_length=50)
+    address_line2 = models.CharField(max_length=50)
+    town_city = models.CharField(max_length=30)
+    postcode = models.CharField(max_length=10)
+    country = models.CharField(max_length=30)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    product = models.ForeignKey(Products, null=False)
+    quantity = models.IntegerField(blank=False)
