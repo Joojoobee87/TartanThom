09444dfa0588dd8da94015c9ebcaf22cbb97a02c
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-03 16:51:00 +0000

    added stripe keys as environment variables and passed them to the checkout view context

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from checkout.forms import OrderForm
 from django.utils import timezone
 from django.contrib import messages
+import os
 
 # Create your views here.
 
@@ -14,9 +15,18 @@ def checkout(request):
         if order_form.is_valid():
             order = order_form.save()
             order.date = timezone.now()
+            print(order_form.errors)
+            messages.error(request, "Thank you for your order")
+            return redirect(request, 'products')
         else:
             print(order_form.errors)
             messages.error(request, "Please review your details")
     else:
         order_form = OrderForm()
-    return render(request, 'checkout/checkout.html', {'order_form': order_form})
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': os.environ.get('STRIPE_PUBLISHABLE'),
+            'client_secret': os.environ.get('STRIPE_SECRET'),
+        }
+
+    return render(request, 'checkout/checkout.html', context)
