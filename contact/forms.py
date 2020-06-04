97ce0d23d1c8704c1fdf31a0e6562621a2be1830
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-04 16:18:21 +0000

    amended form to change name field to subject

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=80)
+    subject = forms.CharField(label='Subject', max_length=80)
     email = forms.EmailField(label='Email')
     message = forms.CharField(label='Your Message')
