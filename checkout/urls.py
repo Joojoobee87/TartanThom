2cda8a9a0d1aae27be973039944fe84090a7beef
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-05 15:23:49 +0000

    added path to checkout success page

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from checkout.views import checkout
+from checkout.views import checkout, checkout_success
 
 app_name = 'checkout'
 urlpatterns = [
-    path('', checkout, name='checkout')
+    path('', checkout, name='checkout'),
+    path('success/', checkout_success, name='checkout_success')
 ]
