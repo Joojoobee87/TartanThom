4ae354d1c4d9f8efc234657dc0dab87301007ad0
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-05 14:33:26 +0000

    added stripe variables and rendered context in checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from checkout.forms import OrderForm
+from .models import Order, OrderItem
+from products.models import Products
 from basket.contexts import basket_contents
 from django.utils import timezone
 from django.contrib import messages
@@ -13,9 +15,11 @@ import stripe
 
 @login_required
 def checkout(request):
+
+    print("1 - Start of function")
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    
+
     basket = request.session.get('basket', {})
     current_contents = basket_contents(request)
     total = current_contents['total']
@@ -26,7 +30,6 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY
     )
     order_form = OrderForm()
-    
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
@@ -36,24 +39,6 @@ def checkout(request):
     return render(request, 'checkout/checkout.html', context)
 
 
-    
-    """if request.method == "POST":
-        order_form = OrderForm(request.POST)
-        if order_form.is_valid():
-            order = order_form.save()
-            order.date = timezone.now()
-            print(order_form.errors)
-            messages.success(request, "Thank you for your order")
-            return redirect(request, 'products')
-        else:
-            print(order_form.errors)
-            messages.error(request, "Please review your details")
-    else:
-        order_form = OrderForm()
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': os.environ.get('STRIPE_PUBLISHABLE'),
-            'client_secret': os.environ.get('STRIPE_SECRET'),
-        }
-
-    return render(request, 'checkout/checkout.html', context)"""
+def checkout_success(request):
+    """Return success page on successful checkout"""
+    return render(request, 'checkout/success.html')
