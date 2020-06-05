5a81b9508482e175ed168a19d53ae8881ad17697
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-05 15:01:45 +0000

    updated name of paths in products urls.py to match view function names

--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,8 @@ from products.views import view_products, view_cards, view_cake_toppers, view_gi
 app_name = 'products'
 urlpatterns = [
     path('', view_products, name='view_products'),
-    path('cards/', view_cards, name='cards'),
-    path('cake-toppers/', view_cake_toppers, name='cake-toppers'),
-    path('gifts/', view_gifts, name='gifts'),
+    path('cards/', view_cards, name='view_cards'),
+    path('cake-toppers/', view_cake_toppers, name='view_cake-toppers'),
+    path('gifts/', view_gifts, name='view_gifts'),
     path('view-product/<id>/', view_product, name='view_product'),
 ]
