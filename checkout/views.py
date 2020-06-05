94fc832817f5de0b4938553ecb546f1da86d8d0f
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-05 09:58:00 +0000

    added stripe variables to checkout function

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from checkout.forms import OrderForm
+from basket.contexts import basket_contents
 from django.utils import timezone
 from django.contrib import messages
 import os
+import stripe
 
 # Create your views here.
 
 
 @login_required
 def checkout(request):
-    if request.method == "POST":
+    stripe_publishable = settings.STRIPE_PUBLISHABLE
+    stripe_secret = settings.STRIPE_SECRET
+    order_form = OrderForm(request.POST)
+    basket = request.session.get('basket', {})
+    current_contents = basket_contents(request)
+    total = current_contents['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+    context = {
+        'order_form': order_form,
+        'stripe_publishable': stripe_publishable,
+        'client_secret': intent.stripe_secret,
+    }
+    print(intent)
+
+    return render(request, 'checkout/checkout.html', context)
+
+
+    
+    """if request.method == "POST":
         order_form = OrderForm(request.POST)
         if order_form.is_valid():
             order = order_form.save()
@@ -29,4 +55,4 @@ def checkout(request):
             'client_secret': os.environ.get('STRIPE_SECRET'),
         }
 
-    return render(request, 'checkout/checkout.html', context)
+    return render(request, 'checkout/checkout.html', context)"""
