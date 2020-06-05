c2db320cdec4e14811ddc32858df04e35ce3d14a
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-05 14:59:35 +0000

    updated url name to match view function

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from products.views import view_products, view_cards, view_cake_toppers, view_gi
 
 app_name = 'products'
 urlpatterns = [
-    path('products/', view_products, name='products'),
+    path('', view_products, name='view_products'),
     path('cards/', view_cards, name='cards'),
     path('cake-toppers/', view_cake_toppers, name='cake-toppers'),
     path('gifts/', view_gifts, name='gifts'),
