a822690b8fa6e04df442eca24c3f99384a0989ad
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-05 11:04:51 +0000

    updated name of stripe keys

--- checkout/views.py
+++ checkout/views.py
@@ -13,24 +13,25 @@ import stripe
 
 @login_required
 def checkout(request):
-    stripe_publishable = settings.STRIPE_PUBLISHABLE
-    stripe_secret = settings.STRIPE_SECRET
-    order_form = OrderForm(request.POST)
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
     basket = request.session.get('basket', {})
     current_contents = basket_contents(request)
     total = current_contents['total']
     stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret
+    stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY
     )
+    order_form = OrderForm()
+    
     context = {
         'order_form': order_form,
-        'stripe_publishable': stripe_publishable,
-        'client_secret': intent.stripe_secret,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
-    print(intent)
 
     return render(request, 'checkout/checkout.html', context)
 
