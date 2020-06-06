4a36cb998e3fdf0064c7e3b5bb18d3851ecea24c
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-06 11:31:00 +0000

    added path to urlpatterns for stripe webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from checkout.views import checkout, checkout_success
+from .webhooks import webhook
 
 app_name = 'checkout'
 urlpatterns = [
     path('', checkout, name='checkout'),
-    path('success/', checkout_success, name='checkout_success')
+    path('success/<order_number>/', checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
