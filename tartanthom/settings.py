ab1c40a92e1e8dca756e7f7c61b6567ee061d776
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-07 16:57:05 +0000

    updated settings for deployment

--- tartanthom/settings.py
+++ tartanthom/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-if 'DEVELOPMENT':
-    import env
 import dj_database_url
-# if os.path.exists("env.py"):
-#     import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
