a801d9a1dd972c14abae7c52c3dba6fcaf01c230
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-07 21:52:24 +0000

    added project dir and static root to fix issue with collectstatic

--- tartanthom/settings.py
+++ tartanthom/settings.py
@@ -258,3 +258,6 @@ else:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
