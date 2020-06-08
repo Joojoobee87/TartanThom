0a89d664d10857fffabf27b706af27d54085884a
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-08 17:39:25 +0000

    updated add files under static directory

---
+++ static/checkout/js/stripe_elements.js
@@ -0,0 +1,58 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+
+// Add Elements to payment page
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+
+var style = {
+    base: {
+        color: "#32325d",
+    }
+};
+
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
+
+// Validation of card element
+
+card.on('change', ({error}) => {
+    const displayError = document.getElementById('card-errors');
+    if (error) {
+        displayError.textContent = error.message;
+    } else {
+        displayError.textContent = '';
+    }
+});
+
+// Complete the payment when the user clicks
+
+var form = document.getElementById('payment-form');
+console.log(form)
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // Show error to your customer (e.g., insufficient funds)
+            const displayError = document.getElementById('card-errors');
+            displayError.textContent = error.message;
+            console.log(result.error.message);
+            card.update({'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
