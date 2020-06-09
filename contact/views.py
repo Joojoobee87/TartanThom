cf5bff7f8af16fb1207cafb9e53fcb5cfb1bdf91
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-09 16:44:49 +0000

    updated contact function removing print and loaded static to html template

--- contact/views.py
+++ contact/views.py
@@ -11,11 +11,11 @@ import os
 
 
 def contact(request):
-    """Submits detail of contact form"""
+    """
+    Gets detail from users contact form and sends to Tartan Thom an email rendering content of form
+    """
     form = ContactForm(request.POST)
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        print("1. I am here")
         subject = request.POST.get('subject')
         message = request.POST.get('message')
         email = request.POST.get('email')
@@ -23,41 +23,23 @@ def contact(request):
         to_email = ['jo.broomfield87@gmail.com']
         message_context = {
             'email': email,
-            'message': message
+            'message': message,
+            'subject': subject
         }
+        header = "You've got mail!"
         html_message = render_to_string('contact/contact_message.html', message_context)
         plain_message = strip_tags(html_message)
-        # create a form instance and populate it with data from the request:
-        # check whether it's valid:
         if form.is_valid():
-            print("2. I am now here")
-
             if subject and message and from_email:
-                print("3. I am here.....")
                 try:
-                    print("4. I am trying....")
-                    print(from_email)
-                    print(to_email)
-                    print(subject)
-                    print(message)
-                    print(html_message)
-                    send_mail(subject, plain_message, from_email, to_email, html_message=html_message, fail_silently=False)
-                    #send_mail(subject, message, from_email, to_email, fail_silently=False)
+                    send_mail(header, plain_message, from_email, to_email, html_message=html_message, fail_silently=False)
                     return redirect(reverse('index'))
-                    # process the data in form.cleaned_data as required
-                    # ...
-                    # redirect to a new URL:
                 except BadHeaderError:
-                    print("5. I have an issue")
                     return HttpResponse('Invalid header found')
                 return render(request, "home/index.html")
             else:
                 form.errors
-                print(form.errors)
-
-            # if a GET (or any other method) we'll create a blank form
     else:
-
         form = ContactForm()
 
     return render(request, "contact/contact.html", {'form': form})
