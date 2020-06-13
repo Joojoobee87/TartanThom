3a07ce37ceb4cb8dfb234a53a73add5464ce9e2a
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-13 14:25:31 +0000

    added verbose name for admin panel

--- home/models.py
+++ home/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 
 class Testimonials(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Testimonials'
+
     testimonial_date = models.DateField()
     testimonial_allow_publish = models.BooleanField(default=True)
     testimonial = models.TextField()
