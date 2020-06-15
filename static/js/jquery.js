6d8ee91653afa066286773841d73bf63c021e388
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-15 16:40:51 +0000

    added styling and jquery function to show messages for 10 seconds before timeout

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,34 +1,46 @@
 $(document).ready(function() {
 
-let index = 0
-let right = $('.button-right');
-let left = $('.button-left');
-let testimonial = $('.testimonial');
-$(testimonial[0]).css("display", "block");
+    // loop through testimonials on homepage
 
-right.on('click', function () {
-    for (i = 0; i < testimonial.length; i++) {
-        $(testimonial[i]).css("display", "none");
-    }
-    index++;
-    if (index > testimonial.length - 1) {
-        $(testimonial[testimonial.length - 1]).css("display", "block");
-        index = 0;
-        return false;
-    }
-    $(testimonial[index - 1]).css("display", "block");
+    let index = 0
+    let right = $('.button-right');
+    let left = $('.button-left');
+    let testimonial = $('.testimonial');
+    $(testimonial[0]).css("display", "block");
+
+    right.on('click', function () {
+        for (i = 0; i < testimonial.length; i++) {
+            $(testimonial[i]).css("display", "none");
+        }
+        index++;
+        if (index > testimonial.length - 1) {
+            $(testimonial[testimonial.length - 1]).css("display", "block");
+            index = 0;
+            return false;
+        }
+        $(testimonial[index - 1]).css("display", "block");
+    })
+
+    left.on('click', function () {
+        for (i = 0; i < testimonial.length; i++) {
+            $(testimonial[i]).css("display", "none");
+        }
+        index--;
+        if (index < 1) {
+            $(testimonial[testimonial.length + 1]).css("display", "block");
+            index = testimonial.length
+        }
+        $(testimonial[index - 1]).css("display", "block");
+    })
+
+    // Timeout Django alerts after 10 seconds
+
+    var alert = document.getElementById("django-alert");
+
+    $(alert).css('display', 'block')
+    setTimeout(function(){ 
+    alert.style.display = "none"; 
+    }, 10000);
+
 })
 
-left.on('click', function () {
-    for (i = 0; i < testimonial.length; i++) {
-        $(testimonial[i]).css("display", "none");
-    }
-    index--;
-    if (index < 1) {
-        $(testimonial[testimonial.length + 1]).css("display", "block");
-        index = testimonial.length
-    }
-    $(testimonial[index - 1]).css("display", "block");
-})
-
-})
