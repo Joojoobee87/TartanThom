7e8cf06e52de4131fc322bb9b0962944921ee209
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-06-19 11:18:05 +0000

    updated setting.py for Heroku deployment

--- tartanthom/settings.py
+++ tartanthom/settings.py
@@ -142,17 +142,17 @@ WSGI_APPLICATION = 'tartanthom.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 # DEBUG TESTING
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-#else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # DEBUG TESTING END
 
@@ -225,32 +225,38 @@ STRIPE_CURRENCY = 'usd'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # Amazon AWS settings
-#AWS_S3_OBJECT_PARAMETERS = {
-#    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-#    'CacheControl': 'max-age=94608000'
-#}
-#AWS_STORAGE_BUCKET_NAME = 'tartanthom'
-#AWS_S3_REGION_NAME = 'eu-west-2'
-#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000'
+}
+AWS_STORAGE_BUCKET_NAME = 'tartanthom'
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
 
 # Static and Media settings when using Amazon AWS
-#STATICFILES_LOCATION = 'static'
-#STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-#MEDIAFILES_LOCATION = 'media'
-#DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 # Override static and media URLs in production
-#STATIC_URL = "/static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "node_modules")]
-#MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "node_modules")]
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
-#Static and Media settings when in development
+# Static and Media settings when in development
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Formatting of Date Fields
+USE_L10N = True
+DATE_INPUT_FORMATS = (
+    '%d-%m-%y', '%d/%m/%y', '%d-%m-%Y', '%d %m %Y', '%Y-%m-%d'
+)
